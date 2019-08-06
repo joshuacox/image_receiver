cad953817f565794ad52987d3a0f7b84dd4b573c
Josh Cox <josh@webhosting.coop>
2019-08-06 17:29:34 -0500

    hh

--- opencv/start.sh
+++ opencv/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ~/.bashrc
 RANDO=$(python -c 'import os; print(os.urandom(16))')
+cd /usr/srv/app
 printf "SECRET_KEY = %s" $RANDO > config.py
 waitress-serve --call 'flaskr:create_app'
